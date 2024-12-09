952c07f72065ed048e26f8d7f8114c80948b2012
hacarapi <huberacarapi@gmail.com>
2024-12-09 01:36:51 -0400

    Entrega Final

--- ecoapp/settings.py
+++ ecoapp/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'estacionamiento'
+    'estacionamiento',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
